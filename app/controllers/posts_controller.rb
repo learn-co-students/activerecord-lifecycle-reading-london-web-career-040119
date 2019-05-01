2a860d6aa9f9705ffc1f4ef33aab2fda11914032
github username <github email address>
2019-05-01 13:17:09 +0100

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,8 @@ class PostsController < ApplicationController
 
 	def update
 	  @post = Post.find(params[:id])
-	  @post.update(params.require(:post))
+	  @post.update(params)
+		byebug
 	  redirect_to post_path(@post)
 	end
 
